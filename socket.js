4f886b7e9f8e14f641b45f1740cad83a6f414829
269043389 <269043389@qq.com>
2017-03-27 21:27:20 +0800

    init laravel5.2

---
+++ socket.js
@@ -0,0 +1,45 @@
+//#######测试1
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var Redis = require('ioredis');
+var redis = new Redis('6379','139.199.8.48',{ password: 'hi,redis' });
+app.listen(3000, function() {
+    console.log('Server is running! Listening on Port 3000');
+});
+function handler(req, res) {
+    res.writeHead(200);
+    res.end('');
+}
+io.on('connection', function(socket) {
+    console.log('connected');
+});
+redis.psubscribe('*', function(err, count) {
+    console.log('err:'+err+"/count:"+count);
+});
+redis.on('pmessage', function(subscribed, channel, message) {
+    console.log(subscribed);
+    console.log(channel);
+    console.log(message);
+
+    message = JSON.parse(message);
+    console.log('message:'+message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+
+//######测试2
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var Redis = require('ioredis');
+// var redis = new Redis('6379','139.199.8.48',{ password: 'hi,redis' });
+//
+// redis.subscribe('test-channel', function(err, count) {
+// });
+// redis.on('message', function(channel, message) {
+//     console.log('Message Recieved: ' + message);
+//     //message = JSON.parse(message);
+//     io.emit(channel + ':' + message.event, message.data);
+// });
+// http.listen(3000, function(){
+//     console.log('Listening on Port 3000');
+// });
